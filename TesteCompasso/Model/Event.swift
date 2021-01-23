4d06184380604ef3dcc91f522af0f44d9333c1d6
Pedro Henrique <pedro.henrique.5566@gmail.com>
2021-01-23 18:58:35 -0300

    Created some key files
    Both Event and People models are created; ViewCodable protocol created.

---
+++ TesteCompasso/Model/Event.swift
@@ -0,0 +1,20 @@
+//
+//  Event.swift
+//  TesteCompasso
+//
+//  Created by Pedro Henrique Cavalcante de Sousa on 23/01/21.
+//
+
+import Foundation
+
+struct Event: Codable {
+    let people: [People]
+    let date: Int
+    let description: String
+    let image: String
+    let longitude: Double
+    let latitude: Double
+    let price: Float
+    let title: String
+    let id: Int
+}
---
+++ TesteCompasso/Model/People.swift
@@ -0,0 +1,14 @@
+//
+//  People.swift
+//  TesteCompasso
+//
+//  Created by Pedro Henrique Cavalcante de Sousa on 23/01/21.
+//
+
+import Foundation
+
+struct People: Codable {
+    let eventId: Int
+    let name: String
+    let email: String
+}
