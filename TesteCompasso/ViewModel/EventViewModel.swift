bce33c80e14b325c720f0e5bf7c400cbd0c65b2a
Pedro Henrique <pedro.henrique.5566@gmail.com>
2021-01-25 16:44:50 -0300

    Adding the initializer from EventDetail to the EventViewModel

--- TesteCompasso/ViewModel/EventViewModel.swift
+++ TesteCompasso/ViewModel/EventViewModel.swift
@@ -30,4 +30,21 @@ struct EventViewModel {
         self.time = dateFormatter.string(from: dateFromEpochTime)
         
     }
+    
+    init(event: EventDetail) {
+        self.title = event.title
+        self.id = event.id
+        
+        let dateFromEpochTime = Date(timeIntervalSince1970: TimeInterval(event.date))
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = NSLocale.current
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        
+        self.date = dateFormatter.string(from: dateFromEpochTime)
+        
+        dateFormatter.dateFormat = "HH:mm"
+        
+        self.time = dateFormatter.string(from: dateFromEpochTime)
+        
+    }
 }
